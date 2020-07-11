ce3eba32606aa3fdf0bf3c99801a04ef738ad044
Yoshida <qubic.co.ltd@gmail.com>
2020-07-11 13:10:12 +0900

    トップ PC 完成

--- js/myscript.js
+++ js/myscript.js
@@ -21,10 +21,25 @@ $(function () {
 	$(window).on('scroll load', function () {
 		let scrollPos = $(window).scrollTop();
 		if (scrollPos > 300) {
-			$('header').addClass('active');
+			$('header').addClass('active').css('transition','.3s');
 		} else {
 			$('header').removeClass('active');
 		}
+
+		$('.fadein').each(function () {
+			var position = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > position - windowHeight + 200) {
+				$(this).addClass('active');
+			}
+		});
+
+
+	});
+
+	$(window).on('load', function () {
+		$('aside,.top01,header').addClass('is_show');
 	});
 
 	$('.top02 h2').click(function () {
